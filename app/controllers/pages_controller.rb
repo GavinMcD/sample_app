f93462d881e952bf0126d0ca1a55dd0d0a453e19
Gavin McDermott <gavin@brightmonk.com>
2011-06-17 15:31:23 -0700

    Added a site layout

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,15 @@ class PagesController < ApplicationController
   # RoR tut: likes to see which actions are defined within the controller
   
   def home
+    @title = "Home"
   end
 
   def contact
+    @title = "Contact"
+  end
+  
+  def about
+    @title = "About"
   end
 
 end
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,11 +1,25 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'home'
+      response.should have_selector("title", 
+                        :content => "Ruby on Rails Tutorial Sample App | Home")
+    end
+    
+    it "should have a non-blank body" do
+      get 'home'
+      # should not match any open body tag with only whitespace characters
+      # only have to do this once because each template pulls from the yield
+      response.body.should_not =~ /<body>\s*<\/body>/
     end
   end
 
@@ -13,6 +27,25 @@ describe PagesController do
     it "should be successful" do
       get 'contact'
       response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'contact'
+      response.should have_selector("title", 
+                        :content => "Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
+
+  describe "GET 'about'" do
+    it "should be successful" do
+      get 'about'
+      response.should be_success
+    end
+    
+    it "should have the right title" do
+      get 'about'
+      response.should have_selector("title", 
+                        :content => "Ruby on Rails Tutorial Sample App | About")
     end
   end
 
