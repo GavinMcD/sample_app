c6697124b39f4b1c1cb62c7b454170eb8216b77c
Gavin McDermott <gavin@brightmonk.com>
2011-07-01 11:13:16 -0700

    Finished the password machinery

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
 class User < ActiveRecord::Base
@@ -23,6 +24,37 @@ class User < ActiveRecord::Base
                     :uniqueness => { :case_sensitive => false }
   validates :password,  :presence => true,
                         :confirmation => true, 
-                        :length => { :within => 6..40 } 
+                        :length => { :within => 6..40 }
+  
+  before_save :encrypt_password
+  
+  def has_password?(submitted_password)
+    encrypted_password == encrypt(submitted_password)
+  end
+  
+  def User.authenticate(email, submitted_password)
+    user = User.find_by_email(email)   # can omit the User in this line
+    return nil  if user.nil?
+    return user if user.has_password?(submitted_password)
+  end
+  
+  private
+  
+    def encrypt_password
+      self.salt = make_salt if self.new_record?
+      self.encrypted_password = encrypt(self.password)
+    end
+  
+    def encrypt(string)
+      secure_hash("#{self.salt}--#{string}")
+    end
+    
+    def make_salt
+      secure_hash("#{Time.now.utc}--#{self.password}")
+    end
+    
+    def secure_hash(string)
+      Digest::SHA2.hexdigest(string)
+    end
   
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -116,8 +116,51 @@ describe User do
       @user.should respond_to(:encrypted_password)
     end
     
+    it "should set the encrypted_password attribute" do
+      @user.encrypted_password.should_not be_blank
+    end
+    
+    # since we're adding a salt attribute, we need to run a migration
+    # add_salt_to_users salt:string
+    it "should have a salt" do
+      @user.should respond_to(:salt)
+    end
+    
+    describe "has_password? method" do
+      it "should exist" do
+        @user.should respond_to(:has_password?)
+      end
+      
+      it "should return true if the passwords match" do
+        @user.has_password?(@attr[:password]).should be_true
+      end
+      
+      it "should return false if the passwords do not match" do
+        @user.has_password?("invalid").should be_false
+      end
+    end
+    
+    describe "authenticate method" do
+      
+      it "should exist" do
+        User.should respond_to(:authenticate)
+      end
+      
+      it "should return nil on email/password mismatch" do
+        User.authenticate(@attr[:email], "wrongpassword").should be_nil
+      end
+      
+      it "should return nil for an email address with no user" do
+        User.authenticate("bar@foo.com", @attr[:password]).should be_nil
+      end
+      
+      it "should return the user on a match" do
+        User.authenticate(@attr[:email], @attr[:password]).should == @user
+      end
+    end
   end
 end
+
 
 # == Schema Information
 #
@@ -129,5 +172,6 @@ end
 #  created_at         :datetime
 #  updated_at         :datetime
 #  encrypted_password :string(255)
+#  salt               :string(255)
 #
 
