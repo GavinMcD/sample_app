0a4ddb6a02cd36dce071751bd9731fa42a723424
Gavin McDermott <gavin@brightmonk.com>
2011-07-05 11:32:34 -0700

    Finished User signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,5 +9,17 @@ class UsersController < ApplicationController
     @user = User.new
     @title = "Sign Up"
   end
+  
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      flash[:success] = "Welcome to the sample app!"
+      redirect_to user_path(@user)   # or: redirect_to @user if you want to shorten
+      # redirect_to @user, flash => { :success => "Welcome to the sample app!" }
+    else      
+      @title = "Sign up"
+      render 'new'
+    end
+  end
 
 end
